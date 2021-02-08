64d02e22ab27ccdf0b03752026b2867125548831
lee <lee.hudson1384@yahoo.co.uk>
2021-02-08 15:17:28 +0000

    Initial commit

---
+++ atmega328-1mhz-blink/.vscode/avr.properties.mk
@@ -0,0 +1,4 @@
+AVR.source.compiler = /usr/bin/avr-gcc
+AVR.source.libraries = 
+AVR.device.type = atmega328p
+AVR.device.frequency = 1000000UL
---
+++ atmega328-1mhz-blink/blink.c
@@ -0,0 +1,23 @@
+
+/*  Upload using arduino as ISP:
+    Upload the example ArduinoISP to the board that will be the programmer, just like any other sketch
+    Connect via ISP to target
+    Run:
+    avrdude -C/usr/share/arduino/hardware/tools/avrdude.conf -v -v -v -v -patmega328p -cstk500v1 -P/dev/ttyACM2 -b19200 -Uflash:w:blink.hex
+*/
+#define F_CPU 16000000  // by default we have the 1MHz internal oscillator
+
+#include <avr/io.h>      // this contains all the IO port definitions
+#include <util/delay.h>
+
+int main(void) {
+  DDRB = (1 << 5);  // set PORTB5 to an output
+
+  while (1) {
+    PORTB |= 1 << 5;  // turn LED on
+    _delay_ms(500);
+    PORTB &= ~(1 << 5);  // turn LED on
+    _delay_ms(500);
+  }
+}
+
